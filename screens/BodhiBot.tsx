fbc81f7a48ebc7aeaaff0d3fb109d4c76daff5dc
Arian Hanifi <counterstrike1337@hotmail.com>
2023-11-24 19:19:25 +0100

    temp workaround for unscrollabel area

--- screens/BodhiBot.tsx
+++ screens/BodhiBot.tsx
@@ -7,6 +7,7 @@ import {
   Button,
   VStack,
   Spinner,
+  View
 } from "native-base";
 import { IConversation } from "../utils/types";
 import { handleChat } from "../helpers/apiHandlers/handleChat";
@@ -128,8 +129,8 @@ export const BodhiBot: React.FC<{ userEmail: string; route: any }> = ({
     }
   }, [userInput, previousUserInput]); // Depend on both userInput and previousUserInput
 
-  
-   useEffect(() => {
+
+  useEffect(() => {
     if (errorResponse) {
       scrollViewRef.current?.scrollToEnd({ animated: true });
     }
@@ -141,116 +142,128 @@ export const BodhiBot: React.FC<{ userEmail: string; route: any }> = ({
       style={{ flex: 1, paddingTop: 20 }}
       imageStyle={{ opacity: 0.7 }}
     >
-      <ScrollView p={4} ref={scrollViewRef}
-      onContentSizeChange={()=> scrollViewRef.current?.scrollToEnd()}>
-        <VStack justifyContent="center" alignItems="center" pb={20} mt={10}>
-          <Flex
-            flexDir="column"
-            w="fit-content"
-            maxW={"70%"}
-            p={2}
-            mb={3}
-            alignSelf={"flex-start"}
-            borderRadius={10}
-            bgColor={"#fff"}
-            borderWidth={1}
-            borderColor="#000"
-          >
-            <Flex fontSize={15} w="100%">
-              <Text
-                style={{
-                  color: "#444654",
-                  fontFamily: "Quicksand",
-                }}
-              >
-                Welcome to your customized interactive meditation journey! I'm
-                here to help you make efficient and meaningful progress in your
-                meditation practice. Let's get started by understanding your
-                goals. The guided meditations will be tailored to your answers
-                so please take your time to answer thourougly. What are you
-                hoping to achieve with meditation?
-              </Text>
-            </Flex>
-          </Flex>
-          {conversation.length === 0 && !conversation ? (
-            <Text
-              fontSize="xl"
-              fontWeight="bold"
-              color="gray.500"
-              alignSelf="center"
-            >
-              BodhiBot
-            </Text>
-          ) : (
-            conversation &&
-            conversation.map((message) => (
-              <MessageComponent message={message} />
-            )
-            ))}
-        </VStack>
-      </ScrollView>
-
-     
       <Flex
-        position="absolute"
-        bottom={Platform.OS === "ios" ? keyboardOffset : 0}
-        flexDirection="row"
-        alignItems="flex-end"
-        p={2}
-        h={"30%"}
-        w={"100%"}
-        flex={1}
+        style={{
+          backgroundColor: "rgba(255, 255, 255, 0.8)",
+        }}
+        h={"100%"}
       >
-        <Input
-          multiline
-          flex={1}
-          placeholder="Type your message..."
-          placeholderTextColor={"#1d1d1d"}
-          value={userInput}
-          onChangeText={(text: string) => setUserInput(text)}
-          isDisabled={loadingReponse}
+        <ScrollView p={4} ref={scrollViewRef} style={{ backgroundColor: "rgba(255, 0, 0, 0.8)" }}
+          onContentSizeChange={() => scrollViewRef.current?.scrollToEnd()}>
+          <VStack justifyContent="center" alignItems="center" pb={20} mt={10}>
+            <Flex
+              flexDir="column"
+              w="fit-content"
+              maxW={"70%"}
+              p={2}
+              mb={3}
+              alignSelf={"flex-start"}
+              borderRadius={10}
+              bgColor={"#fff"}
+              borderWidth={1}
+              borderColor="#000"
+            >
+              <Flex fontSize={15} w="100%">
+                <Text
+                  style={{
+                    color: "#444654",
+                    fontFamily: "Quicksand",
+                  }}
+                >
+                  Welcome to your customized interactive meditation journey! I'm
+                  here to help you make efficient and meaningful progress in your
+                  meditation practice. Let's get started by understanding your
+                  goals. The guided meditations will be tailored to your answers
+                  so please take your time to answer thourougly. What are you
+                  hoping to achieve with meditation?
+                </Text>
+              </Flex>
+            </Flex>
+            {conversation.length === 0 && !conversation ? (
+              <Text
+                fontSize="xl"
+                fontWeight="bold"
+                color="gray.500"
+                alignSelf="center"
+              >
+                BodhiBot
+              </Text>
+            ) : (
+              conversation &&
+              conversation.map((message) => (
+                <MessageComponent message={message} />
+              )
+              ))}
+          </VStack>
+        </ScrollView>
+
+
+        <Flex
+          bottom={Platform.OS === "ios" ? keyboardOffset : 0}
+          flexDirection="row"
+          alignItems="flex-end"
+          p={2}
+          // maxH={"30%"}
+          // minH={"30%"}
+          // w={"100%"}
+          flex-flexShrink={0}
           style={{
-            flex: 1,
-            backgroundColor: "#fff",
-            paddingLeft: 10,
-            borderRadius: 30,
-            borderColor: "#096c7d",
-            borderWidth: 1,
-            color: "#1d1d1d",
-            fontFamily: "Quicksand",
-            elevation: 0, // remove shadow for Android
-            shadowColor: "transparent",
-          }}
-        />
-        <Button
-          ml={2}
-          onPress={() => {
-            handleSubmit(userInput);
-          }}
-          isLoading={loadingReponse}
-          style={{
-            backgroundColor: "#096c7d",
-            width: 45,
-            height: 45,
-            borderRadius: 10,
+            backgroundColor: "rgba(255, 0, 255, 0.8)",
           }}
         >
-          <Icon name="send" style={{ margin: 0 }} size={20} color="#fff" />
-        </Button>
-      
-        {errorResponse && (
-        <Button 
-        ml={2}
-        style={{
-          backgroundColor: "#096c7d",
-          width: 45,
-          height: 45,
-          borderRadius: 10,
-        }}
-        onPress={handleRegenerate} title="Click me to regenerate">
-            <Icon name="sync" style={{ margin: 0 }} size={20} color="#fff" />
-        </Button>
-      )}
+          <Input
+            // h={"100%"}
+            multiline
+            flex={1}
+            placeholder="Type your message..."
+            placeholderTextColor={"#1d1d1d"}
+            value={userInput}
+            onChangeText={(text: string) => setUserInput(text)}
+            isDisabled={loadingReponse}
+            style={{
+              flex: 1,
+              backgroundColor: "#fff",
+              paddingLeft: 10,
+              borderRadius: 30,
+              borderColor: "#096c7d",
+              borderWidth: 1,
+              color: "#1d1d1d",
+              fontFamily: "Quicksand",
+              elevation: 0, // remove shadow for Android
+              shadowColor: "transparent",
+            }}
+          />
+          <Button
+          
+            ml={2}
+            onPress={() => {
+              handleSubmit(userInput);
+            }}
+            isLoading={loadingReponse}
+            style={{
+              backgroundColor: "#096c7d",
+              width: 45,
+              height: 45,
+              borderRadius: 10,
+            }}
+          >
+            <Icon name="send" style={{ margin: 0 }}  color="#fff" />
+          </Button>
+
+          {errorResponse && (
+            <Button
+              ml={2}
+              style={{
+                backgroundColor: "#096c7d",
+                width: 45,
+                height: 45,
+                borderRadius: 10,
+              }}
+              onPress={handleRegenerate} title="Click me to regenerate">
+              <Icon name="sync" style={{ margin: 0 }}  color="#fff" />
+            </Button>
+          )}
+        </Flex>
       </Flex>
     </ImageBackground>
   );
